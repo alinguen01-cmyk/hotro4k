2e53424a656fe56db5a8a4738946708e71f858f0
alinguen01-cmyk <alinguen01@gmail.com>
2026-03-15 22:54:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,8 @@ function openStudocu(){
 
 let link = document.getElementById("link").value
 
-if(link == ""){
-alert("Hãy dán link Studocu")
-return
-}
-
-if(!link.includes("studocu")){
-alert("Đây không phải link Studocu")
+if(link === ""){
+alert("Vui lòng dán link Studocu")
 return
 }
 
@@ -16,9 +11,14 @@ window.open(link,"_blank")
 
 }
 
-
 function openCanva(){
 
-window.open("https://www.canva.com","_blank")
+window.open("https://www.canva.com/","_blank")
+
+}
+
+function openFanpage(){
+
+window.open("https://www.facebook.com/profile.php?id=61582234003016&locale=vi_VN","_blank")
 
 }
