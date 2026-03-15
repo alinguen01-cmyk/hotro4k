3f337ac33c170742b130947a5edd396c9bfc54a0
alinguen01-cmyk <alinguen01@gmail.com>
2026-03-15 21:15:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-function download(){
+function openStudocu(){
 
 let link = document.getElementById("link").value
 
-alert("Link bạn nhập: " + link)
+if(link == ""){
+alert("Hãy dán link Studocu")
+return
+}
+
+if(!link.includes("studocu")){
+alert("Đây không phải link Studocu")
+return
+}
+
+window.open(link,"_blank")
+
+}
+
+
+function openCanva(){
+
+window.open("https://www.canva.com","_blank")
 
 }
